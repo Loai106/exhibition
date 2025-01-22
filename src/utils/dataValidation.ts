224b5822eebdccc4c1f95c88381f695e06d06f12
Loai Mekbel <loai@192.168.1.76>
2025-01-22 18:26:59 +0300

    first commit

--- src/utils/dataValidation.ts
+++ src/utils/dataValidation.ts
@@ -16,3 +16,18 @@ export function validateData(user: object) {
 
   return { value, error };
 }
+
+export function validatePaintingData(painting: object) {
+  const schema = Joi.object({
+    painting_name: Joi.string().alphanum().min(2).max(30).required(),
+    description: Joi.string().alphanum().min(10).required(),
+    height: Joi.number().required(),
+    width: Joi.number().required,
+    date: Joi.date(),
+    exhibtion_id: Joi.number().required(),
+    painting_url: Joi.string().uri().required(),
+  });
+
+  const { value, error } = schema.validate(painting);
+  return { value, error };
+}
