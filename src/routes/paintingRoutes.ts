04f8fc06b67a964b8a09ad09aaf86097c3946a5e
Loai Mekbel <loai@192.168.1.53>
2025-02-13 01:09:23 +0300

    adding verfiyToken middleware

--- src/routes/paintingRoutes.ts
+++ src/routes/paintingRoutes.ts
@@ -6,13 +6,14 @@ import {
   deletePainting,
   updatePainting,
 } from "../controllers/paintingDashboardController";
+import { verifyToken } from "../utils/verifyToken";
 
 const paintingRouter = express.Router();
 
 paintingRouter.get("/paintings", getAllPaintings);
 paintingRouter.get("/paintings/:painting_id", getPaintingDetails);
-paintingRouter.post("/paintings/add-painting", addPainting);
-paintingRouter.put("/paintings/update-painting", updatePainting);
-paintingRouter.delete("/paintings/delete/:id", deletePainting);
+paintingRouter.post("/paintings/add-painting", verifyToken, addPainting);
+paintingRouter.put("/paintings/update-painting", verifyToken, updatePainting);
+paintingRouter.delete("/paintings/delete/:id", verifyToken, deletePainting);
 
 export default paintingRouter;
